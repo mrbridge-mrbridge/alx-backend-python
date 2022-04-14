e2ce6cca2b0bd5e907a2c65a85d7f442748798fb
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-14 22:10:17 +0000

    add annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""module adds two float arguments"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    arguments:
+        a: floating type
+        b: floating type
+    return value: floating type
+    """
+    return a + b
