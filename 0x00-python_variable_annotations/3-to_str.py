e0061c31b3b3abe6f1cd35143cebae5dc1244ef1
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-14 22:42:56 +0000

    str repr

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""module returns str repr of a float argument"""
+
+
+def to_str(n: float) -> str:
+    """
+    arguments:
+        n - floating type
+    returns: str type of n
+    """
+
+    return (str(n))
