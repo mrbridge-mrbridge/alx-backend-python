36b00faa566bff45584303502f2ea9678b028b18
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-14 22:36:28 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""module floors a float"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    arguments:
+        n - floating type
+    returns: int type of n
+    """
+    
+    return (math.floor(n))
