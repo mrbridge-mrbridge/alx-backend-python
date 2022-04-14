e1408b810085f3677a52ade9350c8c3628b587f5
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-14 22:26:46 +0000

    concat strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""module concats two string arguments"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    arguments:
+        str1 - string type
+        str2 - string type
+    returns: concat of string type
+    """
+
+    return (str1 + str2)
