96768b8df8868ce3d36e98ce1b7f2bbded4a03f2
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-14 23:29:53 +0000

    List and Union

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module returns sum of mixed list elements"""
+from typing import List, Union
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    arguments:
+        mxd_lst - List of Union of int and float
+    returns: sum of all list elements as float type
+    """
+    
+    ln = len(mxd_lst)
+    summ = 0
+    for i in range(ln):
+        summ += mxd_lst[i]
+    return summ
