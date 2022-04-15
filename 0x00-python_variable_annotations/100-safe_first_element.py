136f5f5ba098ff75e7f3d97d496d5d5766adee0d
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-15 01:40:51 +0000

    NoneType

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""module augments a function with annotations"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """returns lst[0] or None"""
+    
+    if lst:
+        return lst[0]
+    else:
+        return None
