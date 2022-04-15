63d50b26e9b6bf4063eb9a44b471144672642939
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-15 01:02:41 +0000

    callable

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""module multiplies a float by multiplier"""
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float],float]:
+    """
+    arguments:
+        multiplier - floating type
+    returns: a Callable function
+    """
+
+    return lambda x: x * multiplier
