57e4dccb5e1ca722915ea9183314efd7a0ad8b19
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-15 01:25:40 +0000

    duck types

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function annotation"""
+from typing import Iterable, Sequence, List, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    arguments:
+        lst - of type iterable of sequence
+    returns: A list of tuples of sequence and int
+    """
+
+    return [(i, len(i)) for i in lst]
