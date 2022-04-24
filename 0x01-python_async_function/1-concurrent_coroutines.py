5bd41007bdced96bdfd4c4f882b784257d5b5ea5
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-24 01:57:22 +0000

    runtime

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 """ execute multiple coroutines at the same time with async"""
-import asyncio
-import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Measure the runtime"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """return average runtime"""
+    start = time.perf_counter()
+    asyncio.gather(wait_n(n, max_delay))
+    elapsed = time.perf_counter() - start
+    return (elapsed / n)
+    
