247ab7fb49285379d3374b6b8d918655d8a64c88
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-24 01:27:07 +0000

    sorted

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,12 @@
 """ execute multiple coroutines at the same time with async"""
 import asyncio
 import random
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """returns a sorted list of waiting times"""
-    y: list[float] = [await wait_random(max_delay) for _ in range(n)]
-    return y
+    y: List[float] = [await wait_random(max_delay) for _ in range(n)]
+    return sorted(y)
