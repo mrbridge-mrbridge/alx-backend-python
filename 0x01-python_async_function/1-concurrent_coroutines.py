6ddc52c4ca28680d463ac2acd307417bdaea9dc4
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-24 01:15:35 +0000

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" execute multiple coroutines at the same time with async"""
+import asyncio
+import random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """returns a sorted list of waiting times"""
+    y = [await wait_random(max_delay) for _ in range(n)]
+    return y
