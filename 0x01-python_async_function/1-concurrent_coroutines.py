4ab90b6a524110ffbc2b94701e779b0c8ad4eedb
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-24 01:17:30 +0000

    annotated types

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,5 +8,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """returns a sorted list of waiting times"""
-    y = [await wait_random(max_delay) for _ in range(n)]
+    y: list[float] = [await wait_random(max_delay) for _ in range(n)]
     return y
